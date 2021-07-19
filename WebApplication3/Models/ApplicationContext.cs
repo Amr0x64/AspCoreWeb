cbfe7eee68053a53e0c2bfa43d247bfc86d1779a
Amro <amir-bilalov@mail.ru>
2021-07-19 16:07:34 +0300

    Логирование

--- WebApplication3/Models/ApplicationContext.cs
+++ WebApplication3/Models/ApplicationContext.cs
@@ -4,18 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace WebApplication3.Models
 {
     public class ApplicationContext : IdentityDbContext<User>
-    {       
+    {
+        private readonly StreamWriter logStream = new StreamWriter("mylog.txt", true);
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
            : base(options)
         {
             Database.EnsureCreated();
         }
-        public DbSet<Product> Products{ get; set; }
+        public DbSet<Product> Products { get; set; }
         public DbSet<BuyProduct> BuyProducts { get; set; }
-     
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.LogTo(logStream.WriteLine, Microsoft.Extensions.Logging.LogLevel.Warning);
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            logStream.Dispose();
+        }
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logStream.DisposeAsync();
+        }
+       
     }
 }
--- WebApplication3/Models/Product.cs
+++ WebApplication3/Models/Product.cs
@@ -3,10 +3,11 @@ using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication3.Models
 {
+   
     public class Product 
     {
         public int Id { get; set; }
