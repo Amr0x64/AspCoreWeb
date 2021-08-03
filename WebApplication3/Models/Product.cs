a2574b97bdd84e18fe56d866f66c950bc217d332
Amro <amir-bilalov@mail.ru>
2021-08-03 23:36:20 +0300

    .

--- WebApplication3/Models/Product.cs
+++ WebApplication3/Models/Product.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication3.Models
 {
@@ -11,9 +12,14 @@ namespace WebApplication3.Models
     public class Product 
     {
         public int ProductId { get; set; }
-        public string Title { get; set; }   
+        [Required]
+        public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
+        public string Category { get; set; }
         public int Count { get; set; }
         public string PathImg { get; set; }
         public string AddUser { get; set; }
