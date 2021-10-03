5911548a854566ad42dd40ca141635c831e7dbdf
Amro <amir-bilalov@mail.ru>
2021-10-03 18:03:46 +0300

    1

--- WebApplication3/wwwroot/js/SearchV2.js
+++ WebApplication3/wwwroot/js/SearchV2.js
@@ -1,31 +1,27 @@
 ﻿let allAddress = JSON.parse(document.querySelector("#adressStatment").innerHTML);
-/*let streetNumList = JSON.parse(document.querySelector("#streetNumber").innerHTML);*/
+//let streetNumList = JSON.parse(document.querySelector("#streetNumber").innerHTML);
 
 let checkFirst = true;
 let inputAddress = '';
 let addressList = [];
 let resultAddressInput = '';
 let fulladdress = '';
-let select2 = $('.select2');
+/*let select2 = $('.select2');*/
 
-$(document).ready(function () { // Инициализация
-    $('.select2').select2({
-        placeholder: "Введите адрес",
-        data: addressList,
-        closeOnSelect: false,
-        tokenSeparators: [',', ' ']
-    }).select2('open');
-});
+$('.select2').select2({
+    placeholder: "Введите адрес",
+    data: addressList,
+    closeOnSelect: false,
+    tokenSeparators: [',', ' ']
+}).select2('open');
 
 function event() {
-    console.log("ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ");
+    console.log("ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ");
     /*select2 = $('.select2');*/
-        $(document).ready(function () { // Запрет закрытия
-            $('.select2').on('select2:closing', function () {
-                $('.select2').select2('open');
-            });
-        });
-    
+    $('.select2').on('select2:closing', function () {
+        $('.select2').select2('open');
+    });
+
     $(document).ready(function () { // Возвращает обьект выбора
         $('.select2').on('select2:select', function (e) {
             inputAddress.value = e.params.data.text;
@@ -36,31 +32,22 @@ function event() {
         $('.select2-search__field').focus(function () {
             inputAddress = document.getElementsByClassName('select2-search__field');
             inputAddress = inputAddress[0];
-            try {
-                inputAddress.oninput = search;
-            }
-            catch(err) {
-
-            }
+            inputAddress.oninput = search;
         });
     });
 }
 
 event();
-//select2.on("change", function (e) {
-//    select2.select2("open");
-//});
+let tempParent = {};
 
-let tempParent = "";
 function search() {
-    console.log("Работает oninput");
     if (inputAddress.value == "") {          //empty input
         console.log("Пустой инпкт");
         addressList = [];
         resultAddressInput = "";
         checkFirst = true;
 
-        select2.select2({
+        $(".select2").select2({
             data: addressList,
             closeOnSelect: false,
             tokenSeparators: [',', ' ']
@@ -75,32 +62,26 @@ function search() {
             for (let address in allAddress) {
                 if ((allAddress[address].level == 5 || allAddress[address].level == 8 || allAddress[address].level == 7) &&
                     (allAddress[address].address_name.toLowerCase().indexOf(resultAddressInput.toLowerCase()) == 0)) {
-
                     //        //for (let parent in allAddress) {
                     //        //    if (allAddress[parent].fias_guid == allAddress[address].parent_id) {
                     //        //        tempParent = `${allAddress[parent].short_type_name} - ${allAddress[parent].address_name}`;
                     //        //        break;
                     //        //    }
                     //        //}
-
-                    addressList.push({ "id": 1, "level": allAddress[address].level, "text": `${tempParent}${allAddress[address].short_type_name} - ${allAddress[address].address_name} `});
+                    tempParent = allAddress.find(p => p.fias_guid == allAddress[address].parent_id);
+                    addressList.push({ "id": 1, "level": allAddress[address].level, "text": `${tempParent.address_name}, ${allAddress[address].short_type_name} - ${allAddress[address].address_name} ` });
                 }
             }
             if (addressList.length != 0) {
                 addressList.sort((prev, next) => next.level - prev.level);
-                //addressList.sort(function (a, b) {
-                //    let atext = a.text.toLowerCase(),
-                //        btext = b.text.toLowerCase();
-                //    if (atext < btext) return -1;
-                //    else if (atext > btext) return 1;
-                //});
-                select2.select2({
+                $(".select2").select2({
                     data: addressList,
                     closeOnSelect: false,
                     tokenSeparators: [',', ' ']
-                }).select2("open").trigger('change');   
-                console.log(select2);
+                }).select2("open").trigger('change');
+                console.log(inputAddress.value);
                 event();
+                console.log(inputAddress.value);
                 checkFirst = false;
             }
         }
@@ -108,12 +89,14 @@ function search() {
             /*searchChild();*/
             console.log("Мортра аутист");
         }
-        else {
-            console.log("Паджик пуджик");
-        }
     }
 }
 
 function searchChild() {
     /*$('.select2').val(null).trigger('change');*/
 }
+
+//let arr = [{ a: 1 }, { a: 1 }, { a: 1 }];
+//let mapped = arr.filter(el => el.a == 1);
+//console.log(mapped);
+
